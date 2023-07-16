839e0e8d64e19f95f3aeb7378e589da93702d8b6
Ruben Cipriano <rubencipriano13@gmail.com>
2023-07-16 03:37:40 +0100

    Fixing Cursor on small screens

--- src/js/cursor.js
+++ src/js/cursor.js
@@ -4,7 +4,8 @@ const positionElement = (e)=> {
     const mouseY = e.clientY;
     const mouseX = e.clientX;
     
-    cursor.style.transform = `translate3d(${mouseX - (cursor.clientWidth / 2)}px, ${mouseY - (cursor.clientHeight / 2)}px, 0)`;            
+    if(cursor)
+        cursor.style.transform = `translate3d(${mouseX - (cursor.clientWidth / 2)}px, ${mouseY - (cursor.clientHeight / 2)}px, 0)`;            
 }
 
 window.addEventListener('mousemove', positionElement)
