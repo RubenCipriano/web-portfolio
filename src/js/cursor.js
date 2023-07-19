f3730b5f5ad7132858153a1440a15b1e1c832968
Ruben Cipriano <rubencipriano13@gmail.com>
2023-07-19 13:52:36 +0100

    Fixing Media Bugs

--- src/js/cursor.js
+++ src/js/cursor.js
@@ -5,7 +5,16 @@ const positionElement = (e)=> {
     const mouseX = e.clientX;
     
     if(cursor)
-        cursor.style.transform = `translate3d(${mouseX - (cursor.clientWidth / 2)}px, ${mouseY - (cursor.clientHeight / 2)}px, 0)`;            
+        cursor.style.transform = `translate3d(${mouseX - (cursor.clientWidth / 2)}px, ${mouseY - (cursor.clientHeight / 2)}px, 0)`;     
+        
+    if(window.innerWidth < 991) {
+        document.getElementsByTagName("html")[0].style.cursor = "default";
+        cursor.style.display = "none";
+    } else {
+        document.getElementsByTagName("html")[0].style.cursor = "none";
+        cursor.style.display = "block";
+    }
 }
 
 window.addEventListener('mousemove', positionElement)
+
